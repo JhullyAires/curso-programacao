05e3bb11c40b752a5b960b57b90d192c5dc7643c
Jhully Aires <jhullyolires@gmail.com>
2021-12-10 14:32:30 -0300

    colocando em um package prório

--- src/com/company/PrimeirosPassos/Casting.java
+++ src/com/company/PrimeirosPassos/Casting.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.PrimeirosPassos;
 
 public class Casting {
     public static void main(String[]args) {
--- src/com/company/PrimeirosPassos/Medidas.java
+++ src/com/company/PrimeirosPassos/Medidas.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.PrimeirosPassos;
 
 import java.util.Locale;
 import java.util.Scanner;
