5e489445b5d8751a8b1c0e7de826ff80f7e551d4
Jhully Aires <jhullyolires@gmail.com>
2021-12-14 16:02:49 -0300

    criando classes

--- src/com/company/PrimeirosPassos/EstruturasRepetitivas/ContagemFor.java
+++ src/com/company/PrimeirosPassos/EstruturasRepetitivas/ContagemFor.java
@@ -2,20 +2,19 @@ package com.company.PrimeirosPassos.EstruturasRepetitivas;
 
 import java.util.Scanner;
 
-public class While {
+public class ContagemFor {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        int x = scanner.nextInt();
+        int n = scanner.nextInt();
         int soma = 0;
-        
-        while (x != 0) {
-        	soma += x;
-            x = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            soma += x;
         }
-        
         System.out.println(soma);
         scanner.close();
     }
-
 }
