1153f4f609f1c0909da2b5336dc2f9755d9b78cf
Jhully Aires <jhullyolires@gmail.com>
2022-01-03 22:48:49 -0300

    iniciando POO

---
+++ src/com/company/PrimeirosPassos/POO/NotPOO/TrianguloSemPOO.java
@@ -0,0 +1,40 @@
+package com.company.PrimeirosPassos.POO.NotPOO;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TrianguloSemPOO {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        double xA, xB, xC, yA, yB, yC;
+
+        System.out.println("Enter the measures of triangle x: ");
+        xA = scanner.nextDouble();
+        xB = scanner.nextDouble();
+        xC = scanner.nextDouble();
+
+        System.out.println("Enter the measures of triangle y: ");
+        yA = scanner.nextDouble();
+        yB = scanner.nextDouble();
+        yC = scanner.nextDouble();
+
+        double p = (xA + xB + xC) / 2.0;
+        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+        p = (yA + yB + yC) / 2.0;
+        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+        System.out.printf("T riangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        } else {
+            System.out.println("Larger area: Y");
+        }
+
+        scanner.close();
+    }
+}
